67328260b745a2b650b68f8b2daca6e7bbedc52a
jeffersonhoffmann <jeffersonhoffmann@ufpr.br>
2025-06-04 03:44:07 -0300

    inicio do front, padronizacao vermelho e cinza

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,15 +2,12 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 
-
-
-
 @Component({
-  selector: 'app-login',
+  selector: 'app-root',
   standalone: true,
   imports: [
     MatFormFieldModule,
@@ -21,26 +18,25 @@ import { MatCardModule } from '@angular/material/card';
     MatCardModule
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-    constructor(private fb: FormBuilder, private router: Router){
-      this.loginForm = this.fb.group({
-        email: [''],
-        senha: ['']
-      })
-    }
-
-    onLogin() {
-    this.router.navigate(['/admin']); // redireciona
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    });
   }
 
+  onLogin() {
+    if (this.loginForm.valid) {
 
+      console.log('Login com:', this.loginForm.value);
+      this.router.navigate(['/admin']);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
-
-
-
-
-
