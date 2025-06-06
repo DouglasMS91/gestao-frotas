2e97804e09c81c645b89cb259f6d01c3508b202b
DouglasMS <58640232+DouglasMS91@users.noreply.github.com>
2025-06-06 13:19:42 -0300

    adicionado formgroup ao dialog.html e ao dialog.ts e também a chamada da função no botão salvar btn_Salvar()

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,20 +1,46 @@
-import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { Component, OnInit } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
 
 @Component({
   selector: 'app-dialog',
+  standalone:true,
   imports: [
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    CommonModule,
+    MatDialogModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.css'
 })
-export class DialogComponent {
 
+export class DialogComponent {
+  cadastrar_veiculo_form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.cadastrar_veiculo_form = this.fb.group({
+      placa: [''],
+      modelo: [''],
+      tipo: [''],
+      ano: [''],
+      quilometragemAtual: [''],
+      status: [''],
+    });
+  }
+
+    btn_Salvar() {
+    console.log(this.cadastrar_veiculo_form.value);
+  }
 }
